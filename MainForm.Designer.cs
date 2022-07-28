3c35e1222615a5dd2261854af97c563027cfcf6a
Roads-To-The-Doors <dimacernovolov@gmail.com>
2022-07-28 15:56:22 +0300

    Добавьте файлы проекта.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace Nagan
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chamber = new System.Windows.Forms.Button();
+            this.shot = new System.Windows.Forms.Button();
+            this.twist = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(144, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 73);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(60, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 73);
+            this.button2.TabIndex = 3;
+            this.button2.TabStop = false;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.Maroon;
+            this.button3.Location = new System.Drawing.Point(18, 107);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 73);
+            this.button3.TabIndex = 3;
+            this.button3.TabStop = false;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.Color.Maroon;
+            this.button4.Location = new System.Drawing.Point(50, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 73);
+            this.button4.TabIndex = 3;
+            this.button4.TabStop = false;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.Color.Maroon;
+            this.button5.Location = new System.Drawing.Point(129, 209);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 73);
+            this.button5.TabIndex = 3;
+            this.button5.TabStop = false;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.ForeColor = System.Drawing.Color.Maroon;
+            this.button6.Location = new System.Drawing.Point(198, 163);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(73, 73);
+            this.button6.TabIndex = 3;
+            this.button6.TabStop = false;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.ForeColor = System.Drawing.Color.Maroon;
+            this.button7.Location = new System.Drawing.Point(204, 80);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(73, 73);
+            this.button7.TabIndex = 3;
+            this.button7.TabStop = false;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Nagan.Properties.Resources.Барабан;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 201);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 2;
+            // 
+            // chamber
+            // 
+            this.chamber.BackColor = System.Drawing.Color.Transparent;
+            this.chamber.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.chamber.Location = new System.Drawing.Point(520, 81);
+            this.chamber.Name = "chamber";
+            this.chamber.Size = new System.Drawing.Size(31, 133);
+            this.chamber.TabIndex = 3;
+            this.chamber.TabStop = false;
+            this.chamber.UseVisualStyleBackColor = false;
+            this.chamber.Click += new System.EventHandler(this.chamber_Click);
+            // 
+            // shot
+            // 
+            this.shot.BackColor = System.Drawing.Color.Transparent;
+            this.shot.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.shot.Location = new System.Drawing.Point(432, 236);
+            this.shot.Name = "shot";
+            this.shot.Size = new System.Drawing.Size(144, 102);
+            this.shot.TabIndex = 4;
+            this.shot.TabStop = false;
+            this.shot.UseVisualStyleBackColor = false;
+            this.shot.Click += new System.EventHandler(this.shot_Click);
+            // 
+            // twist
+            // 
+            this.twist.BackColor = System.Drawing.Color.Transparent;
+            this.twist.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.twist.Location = new System.Drawing.Point(415, 81);
+            this.twist.Name = "twist";
+            this.twist.Size = new System.Drawing.Size(99, 133);
+            this.twist.TabIndex = 5;
+            this.twist.UseVisualStyleBackColor = false;
+            this.twist.Click += new System.EventHandler(this.twist_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Nagan.Properties.Resources.Наган;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(874, 513);
+            this.Controls.Add(this.twist);
+            this.Controls.Add(this.shot);
+            this.Controls.Add(this.chamber);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Nagan";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button chamber;
+        private System.Windows.Forms.Button shot;
+        private System.Windows.Forms.Button twist;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Nagan
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        bool[] ammo =
+        {
+            false, false, false, false, false, false, false
+        };
+
+        SoundPlayer wave = null;
+
+        private bool SetAmmo(bool set)
+        {
+            if (set)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool[] NewAmmo()
+        {
+            int length = ammo.Length;
+
+            Random random = new Random();
+
+            int rand = random.Next(length);
+
+            bool[] am = new bool[length];
+
+            if (rand == 0)
+            {
+                for (int i = 0; i < length; ++i)
+                {
+                    am[i] = ammo[i];
+                }
+            }
+            else
+            {
+                int count = 0;
+
+                for (int i = rand; i < length; ++i)
+                {
+                    am[count] = ammo[i];
+
+                    ++count;
+                }
+
+                for (int i = 0; i < rand; ++i)
+                {
+                    am[count] = ammo[i];
+
+                    ++count;
+                }
+            }
+
+            return am;
+        }
+
+        private void Out()
+        {
+            int i = 0;
+
+            if (ammo[i])
+            {
+                button1.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button1.BackgroundImage = null;
+            }
+
+            ++i;
+
+            if (ammo[i])
+            {
+                button2.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button2.BackgroundImage = null;
+            }
+
+            ++i;
+
+            if (ammo[i])
+            {
+                button3.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button3.BackgroundImage = null;
+            }
+
+            ++i;
+
+            if (ammo[i])
+            {
+                button4.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button4.BackgroundImage = null;
+            }
+
+            ++i;
+
+            if (ammo[i])
+            {
+                button5.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button5.BackgroundImage = null;
+            }
+
+            ++i;
+
+            if (ammo[i])
+            {
+                button6.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button6.BackgroundImage = null;
+            }
+
+            ++i;
+
+            if (ammo[i])
+            {
+                button7.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button7.BackgroundImage = null;
+            }
+        }
+
+        private void Sorted()
+        {
+            int length = ammo.Length;
+
+            bool buffer = ammo[0];
+
+            for (int i = 1; i < length; ++i)
+            {
+                ammo[i - 1] = ammo[i];
+            }
+
+            ammo[length - 1] = buffer;
+        }
+
+        /****************************************************/
+        /****************************************************/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            ammo[i] = SetAmmo(ammo[i]);
+
+            if (ammo[i])
+            {
+                button1.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button1.BackgroundImage = null;
+            }
+
+            wave = new SoundPlayer(Properties.Resources.ЗаряжениеПулей);
+
+            wave.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+
+            ammo[i] = SetAmmo(ammo[i]);
+
+            if (ammo[i])
+            {
+                button2.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button2.BackgroundImage = null;
+            }
+
+            wave = new SoundPlayer(Properties.Resources.ЗаряжениеПулей);
+
+            wave.Play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = 2;
+
+            ammo[i] = SetAmmo(ammo[i]);
+
+            if (ammo[i])
+            {
+                button3.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button3.BackgroundImage = null;
+            }
+
+            wave = new SoundPlayer(Properties.Resources.ЗаряжениеПулей);
+
+            wave.Play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = 3;
+
+            ammo[i] = SetAmmo(ammo[i]);
+
+            if (ammo[i])
+            {
+                button4.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button4.BackgroundImage = null;
+            }
+
+            wave = new SoundPlayer(Properties.Resources.ЗаряжениеПулей);
+
+            wave.Play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i = 4;
+
+            ammo[i] = SetAmmo(ammo[i]);
+
+            if (ammo[i])
+            {
+                button5.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button5.BackgroundImage = null;
+            }
+
+            wave = new SoundPlayer(Properties.Resources.ЗаряжениеПулей);
+
+            wave.Play();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int i = 5;
+
+            ammo[i] = SetAmmo(ammo[i]);
+
+            if (ammo[i])
+            {
+                button6.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button6.BackgroundImage = null;
+            }
+
+            wave = new SoundPlayer(Properties.Resources.ЗаряжениеПулей);
+
+            wave.Play();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int i = 6;
+
+            ammo[i] = SetAmmo(ammo[i]);
+
+            if (ammo[i])
+            {
+                button7.BackgroundImage = Properties.Resources.Патрон;
+            }
+            else
+            {
+                button7.BackgroundImage = null;
+            }
+
+            wave = new SoundPlayer(Properties.Resources.ЗаряжениеПулей);
+
+            wave.Play();
+        }
+        
+        /****************************************************/
+        /****************************************************/
+
+        private void chamber_Click(object sender, EventArgs e)
+        {
+            if (panel1.Visible)
+            {
+                panel1.Visible = false;
+
+                twist.Visible = true;
+
+                shot.Visible = true;
+
+                wave = new SoundPlayer(Properties.Resources.Закрытие);
+            }
+            else
+            {
+                panel1.Visible = true;
+
+                twist.Visible = false;
+
+                shot.Visible = false;
+
+                Out();
+
+                wave = new SoundPlayer(Properties.Resources.Открытие);
+            }
+
+            wave.Play();
+        }
+
+        private void shot_Click(object sender, EventArgs e)
+        {
+            if (ammo[0] == true)
+            {
+                ammo[0] = false;
+
+                wave = new SoundPlayer(Properties.Resources.Выстрел);
+            }
+            else
+            {
+                wave = new SoundPlayer(Properties.Resources.Пустой);
+            }
+            
+            Sorted();
+
+            wave.Play();
+        }
+
+        private void twist_Click(object sender, EventArgs e)
+        {
+            ammo = NewAmmo();
+
+            wave = new SoundPlayer(Properties.Resources.КручениеБарабана);
+
+            wave.Play();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            shot.Visible = false;
+
+            twist.Visible = false;
+        }
+    }
+}
